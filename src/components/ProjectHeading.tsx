e64de7596357206a0cfc16f096c17ef34b9155f4
Dellsony <eng.dellsony@gmail.com>
2024-12-10 21:04:34 +0530

    Initial setup

---
+++ src/components/ProjectHeading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectHeading = () => {
+  return (
+    <div>ProjectHeading</div>
+  )
+}
+
+export default ProjectHeading
---
+++ src/components/TableBoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TableBoard = () => {
+  return (
+    <div>TableBoard</div>
+  )
+}
+
+export default TableBoard
